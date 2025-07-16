adbf63bee1bb5fc67651b14baa3b6df1386cea5b
Sagar <Sagar@LAPTOP-R2HUB5EQ>
2025-07-16 19:01:11 +0530

    first commit

---
+++ JSS_Core_Java_Project/src/com/tnsif/samples/FirstProgram.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.tnsif.samples;
+
+/**
+ * @author Sagar
+ *
+ */
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Welcome to java learning");
+		
+	}
+}
